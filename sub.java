e5684c6a1e1d7c2987d3157add6a132ef64b4671
Abishan-shan <abishanshan61@gmail.com>
2023-01-03 14:55:02 +0530

    changed

---
+++ sub.java
@@ -0,0 +1,10 @@
+class sub
+{
+    public static void main(String args[])
+    {
+        int a,b=10,20;
+        
+        System.out.println(a-b); //summation //rajee
+
+    }
+}
--- sum.java
+++ sum.java
@@ -3,11 +3,8 @@ class sum
     public static void main(String args[])
     {
         int a,b=10,20;
-<<<<<<< HEAD
         int c=6;
-=======
         int v=10;
->>>>>>> 0641be1dae675452bba6e89ed951fb594b35753e
         System.out.println(a+b); //summation //rajee
         System.out.println(a+b+c);
     }
